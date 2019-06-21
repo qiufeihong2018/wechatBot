db7147149fadee33b665e4423609aaa8c663d848
qiufeihong <qiufeihong2018@126.com>
2019-06-21 09:46:29 +0800

    docs(更改配置):

--- config/index.js
+++ config/index.js
@@ -1,12 +1,12 @@
 // 配置文件
 module.exports = {
     // 基础定时发送功能配置项（必填项）
-    NAME: 'Leo_chen', //女朋友备注姓名
-    NICKNAME: 'Leo_chen', //女朋友昵称
-    MEMORIAL_DAY: '2015/04/18', //你和女朋友的纪念日
-    CITY: 'shanghai', //女朋友所在城市
-    LOCATION: "pudong-new-district", //女朋友所在区（可以访问墨迹天气网站后，查询区的英文拼写）
-    SENDDATE: '0 06 8 * * *', //定时发送时间 每天8点06分0秒发送，规则见 /schedule/index.js
+    NAME: '小香猪', //女朋友备注姓名
+    NICKNAME: '小香稞', //女朋友昵称
+    MEMORIAL_DAY: '2017/06/3', //你和女朋友的纪念日
+    CITY: 'zhejiang', //女朋友所在城市
+    LOCATION: "ouhai-district", //女朋友所在区（可以访问墨迹天气网站后，查询区的英文拼写）
+    SENDDATE: '0 20 9 * * *', //定时发送时间 每天8点06分0秒发送，规则见 /schedule/index.js
     ONE: 'http://wufazhuce.com/', ////ONE的web版网站
     MOJI_HOST: 'https://tianqi.moji.com/weather/china/', //中国墨迹天气url
 
